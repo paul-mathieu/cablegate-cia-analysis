e002a0046d2fbb85da98e47638385c991e7e29c8
Paul Mathieu <>
2021-04-30 16:01:08 +0200

    convert data faster

--- convert-data/cablegate.py
+++ convert-data/cablegate.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from tika import parser
 from os import listdir
 from core import *
+import re
 import time
 
 def convert_data(local_path=LOCAL_PATH):
@@ -10,13 +11,24 @@ def convert_data(local_path=LOCAL_PATH):
     data_path = path_source + '/data/cablegate/'
     converted_data_path = path_source + '/data/cablegate-converted/'
     files = [f for f in listdir(data_path) if f[-4:] == '.pdf']
-    # start_time = time.time()
+    len_files = len(files)
+    replace_dict = {
+        '\n\n\n': '',
+        '\n\n':'\n',
+        'S E C R E T': 'SECRET',
+        'C O N F I D E N T I A L': 'CONFIDENTIAL',
+        '[INFORMATION FOR THE REPORT\'S CLASSIFIED ANNEX]': '',
+        '[INFORMATION FOR THE REPORT\'S CLASSIFIED ANNEX.]': ''
+        }
+    re_compile = re.compile("(%s)" % "|".join(map(re.escape, replace_dict.keys())))
+    start_time = time.time()
     index_progression = 0
-    print_progress_bar(index_progression, len(files), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    print_progress_bar(index_progression, len_files, prefix = 'Progress:', suffix = 'Complete', length = 50)
     for file in files:
-        txt_file = open(converted_data_path + file[:-4] + '.txt', 'w')
-        txt_file.write(parser.from_file(data_path + file)['content'].replace('\n\n\n','').replace('\n\n','\n'))
-        txt_file.close()
+        text = re_compile.sub(
+                    lambda mo: replace_dict[mo.string[mo.start(): mo.end()]],
+                    parser.from_file(data_path + file)['content'])
         index_progression += 1
-        print_progress_bar(index_progression, len(files), prefix = 'Progress:', suffix = 'Complete', length = 50)
-        # print(time.time() - start_time)
+        print_progress_bar(index_progression, len_files, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    print(time.time() - start_time)
