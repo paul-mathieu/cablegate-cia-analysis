639999d280f1c88057bee5903dfeee39692c8e82
Paul Mathieu <>
2021-05-05 14:29:04 +0200

    adjustments convert data

--- convert-data/run.py
+++ convert-data/run.py
@@ -3,9 +3,9 @@ import cia
 
 def main():
     print('=== cablegate data ===')
-    cablegate.convert_data()
+    # cablegate.convert_data()
     print('=== cia data ===')
-    # cia.convert_data()
+    cia.convert_data()
 
 if __name__ == "__main__":
     main()
